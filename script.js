206bc1765dd0a71fa5e8bc1c4da5f154533c0bd2
Am1d1v <dimadiman1adima@gmail.com>
2023-12-15 20:19:15 +0300

    .

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 
 
+const ratings = document.querySelectorAll('.rating');
+const sendButton = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+
+
+panel.addEventListener('click', (event) => {
+    
+    console.log(ratings);
+
+    // Submit only for button
+    if(event.target === sendButton){
+        console.log('ok');
+    }
+
+    // Choose define smileface
+    if(event.target.parentNode.classList.contains('rating')){
+        removeActive(event.target.parentNode);
+        event.target.parentNode.classList.add('active')
+    }
+});
+
+// Remove class 'active' of elements in rating array
+function removeActive(){
+    ratings.forEach((ratingElement) => {
+        ratingElement.classList.remove('active')
+    })
+}
